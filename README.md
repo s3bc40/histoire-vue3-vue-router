05aa8a2c896b811100866786712ec274dad67763
s3bc40 <32032033+s3bc40@users.noreply.github.com>
2023-04-17 14:09:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# histoire-vue3-vue-router
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/histoire-vue3-starter-tee3fv)
---
+++ src/TestRouterLink.story.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { AfLinkPrimary } from '@af-lib/ui-lib'
+import { ref } from 'vue'
+import { createRouter, createMemoryHistory } from 'vue-router'
+
+import type { App } from 'vue'
+
+const label = ref<string>('ZeldaLinkToThePast')
+const to = ref('/')
+const size = ref<'sm' | 'md' | 'lg'>('md')
+const showIcon = ref(false)
+const showArrow = ref(false)
+const disabled = ref(false)
+
+const sizeOptions = ['sm', 'md', 'lg']
+
+function setupApp({ app }: { app: App }) {
+  // Router mock
+  app.use(
+    createRouter({
+      history: createMemoryHistory(),
+      routes: [{ path: '/', name: 'home', component: { render: () => null } }],
+    })
+  )
+}
+</script>
+
+<template>
+  <Story
+    title="Atoms/AfLink/AfLinkPrimary"
+    auto-props-disabled
+    :setup-app="setupApp"
+  >
+    <router-link to="/"> Hello </router-link>
+
+    <template #controls>
+      <HstText v-model="label" title="Label" />
+      <HstText v-model="to" title="To" />
+      <HstSelect v-model="size" :options="sizeOptions" title="Size" />
+      <HstCheckbox v-model="showIcon" title="Show icon" />
+      <HstCheckbox v-model="showArrow" title="Show arrow" />
+      <HstCheckbox v-model="disabled" title="Disabled" />
+    </template>
+  </Story>
+</template>
+
+<style scoped></style>
